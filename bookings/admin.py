26c43be2f2bd5f7b20847f32eb2bfeeb0af59ba4
ElinaBoman Boman <elinaholmberg@hotmail.com>
2023-12-13 09:21:20 +0000

    Added list_filter to admin panel to be abel to filter bookings

--- bookings/admin.py
+++ bookings/admin.py
@@ -5,4 +5,5 @@ from .models import EventBooking
 @admin.register(EventBooking)
 class EventBookingAdmin(admin.ModelAdmin):
     list_display = ("user", "event_date", "lesson_time","event_choice", "booking_status")
+    list_filter = ('booking_status', 'created_on')
     search_fields = ("user", "event_date", "booking_status")
