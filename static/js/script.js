d1c3df2c4349e885c1ea08a270b2fa6b3e7b7754
ElinaBoman Boman <elinaholmberg@hotmail.com>
2024-01-02 10:36:44 +0000

    Conected contactform with email function

---
+++ static/js/script.js
@@ -0,0 +1,19 @@
+
+
+
+function sendMail(contactForm) {
+    emailjs.send("service_supy7sf", "template_tgz8lrr", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.email.value,
+        "message": contactForm.message.value
+    })
+.then(
+    function(response) {
+        console.log("SUCCESS", response);
+    },
+    function(error) {
+        console.log("FAILED", error);
+    });
+
+    return false;
+}
