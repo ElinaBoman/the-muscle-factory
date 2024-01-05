a1b95aa61f982db81c6a28d979cba6b877917436
ElinaBoman Boman <elinaholmberg@hotmail.com>
2024-01-05 13:07:44 +0000

    Used javascript to send email instead of views and created my own alert message to confirm that the email has been sent

--- contact/views.py
+++ contact/views.py
@@ -4,15 +4,16 @@ from django.contrib import messages
 from .forms import ContactForm
 # Create your views here.
 
+
+
 def contact(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
-            print('form is valid')
             messages.success(request, 'Your message has been sent successfully!')
             return redirect('contact')
         else: form = ContactForm()
 
     else:
         form = ContactForm()
-    return render(request, 'contact/contact.html', {form: form})
+    return render(request, 'contact/contact.html', {'form': form})
--- static/js/script.js
+++ static/js/script.js
@@ -1,21 +1,31 @@
 
+const emailSuccess = document.getElementById("email-success")
+
+document.addEventListener("DOMContentLoaded", function() {
+    const emailSuccess = document.getElementById("email-success");
+    emailSuccess.classList.add("hide");
+});
+
 
 function sendMail(contactForm) {
-
     emailjs.send("service_supy7sf", "template_tgz8lrr", {
     "from_name": contactForm.name.value,
     "from_email": contactForm.email.value,
     "message": contactForm.message.value,
     })
     .then(
-        function(response){
-            console.log("SUCCESS", response);
+        function(response) {
+            console.log("Mail sent successfully:", response);
+            contactForm.reset();
+            emailSuccess.classList.remove('hide');
+            setTimeout(function() {
+                emailSuccess.classList.add('hide');
+            }, 2000);   
         },
         function(error) {
-            console.log("FAILED", error);
+            console.log("Error sending mail:", error);
         }
-        
     );
+    return false
 }
 
- 
