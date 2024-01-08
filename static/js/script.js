f11dc97e5b471c1a05624e5ffbfe32120be28776
ElinaBoman Boman <elinaholmberg@hotmail.com>
2024-01-08 10:13:33 +0000

    Corrected sendEmail function

--- static/js/script.js
+++ static/js/script.js
@@ -1,37 +1,33 @@
 
 
-var bootstrap_alert = function() {};
-
-bootstrap_alert.error = function(message) {
-    $('#alert_placeholder').html('<div class="alert alert-danger"><a class="close" data-dismiss="alert">×</a><span>' + message + '</span></div>');
-}
-bootstrap_alert.success = function(message) {
-    $('#alert_placeholder').html('<div class="alert alert-success"><a class="close" data-dismiss="alert">×</a><span>' + message + '</span></div>');
-}
-
-
-    function sendMail(contactForm) {
-        emailjs.send("service_supy7sf", "template_tgz8lrr", {
-            "from_name": contactForm.name.value,
-            "from_email": contactForm.email.value,
-            "message": contactForm.message.value,
-        })
-        .then(
-            function(response) {
-                console.log("Mail sent successfully:", response);
-                bootstrap_alert.success('Mail sent succesfully!');
-                contactForm.reset();
-                },
-            function(error) {
-                console.log("Error sending mail:", error);
-                bootstrap_alert.error('Mail was not sent');
-                }
-            );
-        return false;
+class bootstrap_alert {
+    constructor() { }
+    static success(message) {
+        $('#alert_placeholder').html('<div class="alert alert-success"><a class="close" data-bs-dismiss="alert">×</a><span>' + message + '</span></div>');
     }
-document.querySelector('.contactForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    sendMail(this);
-});
+    static error(message) {
+        $('#alert_placeholder').html('<div class="alert alert-danger"><a class="close" data-bs-dismiss="alert">×</a><span>' + message + '</span></div>');
+    }
+}
 
+function sendMail(contactForm) {
+    emailjs.send("service_ogoy0i8", "template_xb922ko", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.email.value,
+        "message": contactForm.message.value,
+    })
+    .then(
+        function(response) {
+            console.log("Mail sent successfully:", response);
+            bootstrap_alert.success('Mail sent succesfully!');
+            contactForm.reset();
+            },
+    
+        function(error) {
+            console.log("Error sending mail:", error);
+            bootstrap_alert.error('Mail was not sent');
+            }
+        );
+    return false
+}
 
