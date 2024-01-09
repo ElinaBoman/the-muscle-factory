e5e76130498fa8b552240e0c981af4061e6ac3e0
ElinaBoman Boman <elinaholmberg@hotmail.com>
2024-01-09 08:12:38 +0000

    Set debug to false

--- themusclefactory/settings.py
+++ themusclefactory/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['the-muscle-factory-9f171161969d.herokuapp.com', '8000-elinaboman-themusclefac-afzywqqs6lt.ws-eu106.gitpod.io', '8000-elinaboman-themusclefac-afzywqqs6lt.ws-eu107.gitpod.io']
 
