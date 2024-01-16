67226bb20f93cd2a14e52f6f20c8914901e33207
ElinaBoman Boman <elinaholmberg@hotmail.com>
2024-01-16 13:31:25 +0000

    Testing deployment

--- static/js/script.js
+++ static/js/script.js
@@ -17,7 +17,7 @@ contactForm.addEventListener('submit', function(event) {
 event. If event is successfull the contactForm will execute submit().*/
 function sendMail(contactForm) {
     
-    emailjs.send("service_ogoy0i8", "template_xb922ko", {
+    emailjs.send("service_supy7sf", "template_tgz8lrr", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.email.value,
         "message": contactForm.message.value,
