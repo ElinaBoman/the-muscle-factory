a162e34906f4f3898cebac3048a680919e5cbe9f
ElinaBoman Boman <elinaholmberg@hotmail.com>
2024-01-21 10:13:44 +0000

    Adjusted bootstrap classes in about page, removed unnecessary classes

--- contact/models.py
+++ contact/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 
 class ContactForm(models.Model):
+    '''
+    Contact form model.
+    '''
     name = models.CharField(max_length=50, blank=False)
     lastname = models.CharField(max_length=50, blank=True, null=True)
     phonenumber = models.IntegerField(blank=True, null=True)
