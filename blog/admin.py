fe69a59dca4904ffc01e0d7367d9337066543f32
ElinaBoman Boman <elinaholmberg@hotmail.com>
2024-01-25 07:27:37 +0000

    Corrected code in blog: admin.py and added comment

--- blog/admin.py
+++ blog/admin.py
@@ -5,13 +5,13 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Event)
 class EventAdmin(SummernoteModelAdmin):
-    prepopulated_fields={'slug': ('title',)}
-    list_filter=('status', 'created_on',)
+    '''
+    Fileds will show up in Admin panel. This code has been copied
+    from Code Institutes walktrough project:
+    https://github.com/Code-Institute-Solutions/Django3blog/tree/master/11_messages/blog
+    '''
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on',)
     summernote_fields = ('content')
     list_display = ('title', 'slug', 'created_on')
     search_fields = ['title', 'content']
-
-
-# This is for admin to see members and administrate members
-
-
